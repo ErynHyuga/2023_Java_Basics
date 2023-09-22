257c8e58962369e8038dd40948471e125c7f615d
ErynHyuga <you@example.com>
2023-09-22 08:56:43 +0200

    if loop 01 started and finished

---
+++ src/at/kogler/exercise04/main_loop_01.java
@@ -0,0 +1,11 @@
+package at.kogler.exercise04;
+
+public class main_loop_01 {
+    public static void main(String[] args) {
+        int k = 0;
+        for (int i = 1; i < 101; i++) {
+            k = i + k;
+            System.out.println(k);
+        }
+    }
+}
