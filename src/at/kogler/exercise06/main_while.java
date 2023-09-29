c598767182b9a58557dadea55e0a4a55ad925a0b
ErynHyuga <you@example.com>
2023-09-29 08:09:22 +0200

    while loop finished

--- src/at/kogler/exercise06/main_while.java
+++ src/at/kogler/exercise06/main_while.java
@@ -2,10 +2,10 @@ package at.kogler.exercise06;
 import java.util.Random;
 public class main_while {
     public static void main(String[] args) {
-        boolean checker = true;
+        boolean checkerTrue = true;
 
         int k = 0;
-        while (checker){
+        while (checkerTrue){
 
             Random RNG = new Random();
             int RN = RNG.nextInt(10, 30 );
@@ -14,7 +14,7 @@ public class main_while {
             System.out.println(k);
 
             if (RN == 15 || RN == 25){
-            checker = false;
+            checkerTrue = false;
             }
         }
     }
