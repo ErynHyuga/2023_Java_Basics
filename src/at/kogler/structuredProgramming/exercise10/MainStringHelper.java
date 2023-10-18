3e6655863a548d9480fdd536e535aeffb1ed4844
ErynHyuga <you@example.com>
2023-10-18 09:32:50 +0200

    first real attempt at stringhelper

--- src/at/kogler/structuredProgramming/exercise10/MainStringHelper.java
+++ src/at/kogler/structuredProgramming/exercise10/MainStringHelper.java
@@ -1,8 +1,20 @@
 package at.kogler.structuredProgramming.exercise10;
+import java.util.Scanner;
+
 
 public class MainStringHelper {
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        Scanner scanInput = new Scanner(System.in);
+
+        System.out.println("Input Word: ");
+
+        String inFromConsole = scanInput.next();
+
+        if(inFromConsole.charAt(0)==inFromConsole.charAt(inFromConsole.length()-1)) {
+            System.out.println("test");
+        }
 
     }
+
 }
