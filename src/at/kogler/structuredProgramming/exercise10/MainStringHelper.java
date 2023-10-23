d8e1c88ddd28f97301bcaa8dfc1370493b913c92
ErynHyuga <you@example.com>
2023-10-23 14:24:11 +0200

    Strinhelper is finished i guess

--- src/at/kogler/structuredProgramming/exercise10/MainStringHelper.java
+++ src/at/kogler/structuredProgramming/exercise10/MainStringHelper.java
@@ -1,20 +1,13 @@
 package at.kogler.structuredProgramming.exercise10;
 import java.util.Scanner;
-
-
 public class MainStringHelper {
-
     public static void main(String[] args) {
-        Scanner scanInput = new Scanner(System.in);
-
-        System.out.println("Input Word: ");
-
-        String inFromConsole = scanInput.next();
-
-        if(inFromConsole.charAt(0)==inFromConsole.charAt(inFromConsole.length()-1)) {
-            System.out.println("test");
-        }
-
+        var scanInput = new Scanner(System.in);
+        System.out.println(StringHelperChecker.countLetters("Hello World!", 'l'));
+        String input = scanInput.nextLine();
+        System.out.println(StringHelperChecker.isPalindrome(input));
+        System.out.println(StringHelperChecker.countLetters(input, 'l'));
+        System.out.println(StringHelperChecker.reverseString(input));
+        StringHelperChecker.printAmountOfLetters(input);
     }
-
 }
---
+++ src/at/kogler/structuredProgramming/exercise10/StringHelperChecker.java
@@ -0,0 +1,48 @@
+package at.kogler.structuredProgramming.exercise10;
+
+public class StringHelperChecker {
+    public static boolean isPalindrome(String input) {
+        for (int i = 0; i < input.length() / 2; i++) {
+            if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static int countLetters(String input_string, char input_char) {
+        int count = 0;
+
+        for (int i = 0; i < input_string.length(); i++) {
+            if (input_string.charAt(i) == input_char) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static String reverseString(String input) {
+        String output = "";
+        for (int i = 0; i < input.length(); i++) {
+            char character = input.charAt(i);
+            output = character + output;
+        }
+        return output;
+    }
+
+    public static void printAmountOfLetters(String input) {
+        int[] character_list = new int[256]; // Assuming ASCII characters
+
+        for (int i = 0; i < input.length(); i++) {
+            char character = input.charAt(i);
+            character_list[character]++;
+        }
+
+        for (int i = 0; i < character_list.length; i++) {
+            int amount = character_list[i];
+            if (amount > 0) {
+                System.out.println((char)i + ": " + amount);
+            }
+        }
+    }
+}
