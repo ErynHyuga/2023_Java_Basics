8fad75635ebfbbfada40b93ec092524137fc3b8a
ErynHyuga <you@example.com>
2023-11-13 14:20:01 +0100

    Continued to work on the car. Started with the honk and repitition

--- src/at/kogler/oOProgramming/exercise01/CarBrummBrumm.java
+++ src/at/kogler/oOProgramming/exercise01/CarBrummBrumm.java
@@ -1,4 +1,5 @@
 package at.kogler.oOProgramming.exercise01;
+
 import java.util.Scanner;
 
 public class CarBrummBrumm {
@@ -10,39 +11,48 @@ public class CarBrummBrumm {
     public int carMaxFuelStorage;
     int speed;
 
-    public void carMaxFuelChecker(){
-        if (carFuelInStorage > carMaxFuelStorage){
+    public void carMaxFuelChecker() {
+        if (carFuelInStorage > carMaxFuelStorage) {
             carFuelInStorage = carMaxFuelStorage;
         }
     }
 
     public void carBeFastAsFuckBoyyyyAKADriving() {
-        if(carFuelInStorage > 0){
+        if (carFuelInStorage > 0) {
             System.out.println("'Cause I'm crazy, hot and ready, but you like it\n" +
                     "I wanna race for you (Shall I go now?)\n" +
                     "Gas, gas, gas\n" +
                     "I'm gonna step on the gas");
             this.carFuelInStorage = this.carFuelInStorage - carFuelConsumption;
-
+        } else {
+            System.out.println("Car Cannot Move. Not Enough Fuel");
         }
     }
 
     public void carGoSlowAKABreak() {
         System.out.println("I Break");
     }
-    public void carGoSuperBoostModeZefix(){
-        if (carMaxFuelStorage / 10 < carFuelInStorage ) {
-            System.out.println("very fast");
 
+    public void carGoSuperBoostModeZefix() {
+        if (carMaxFuelStorage / 10 < carFuelInStorage) {
+            System.out.println("very fast");
+        } else {
+            System.out.println("Not Enough Fuel for this");
         }
 
     }
-    public void carGoHonk(){
+
+    public void carGoHonk() {
         var scanInput = new Scanner(System.in);
         int inputHonkRepitition = Integer.parseInt(scanInput.next());
-        for (){
+        for (int i = 0; inputHonkRepitition >= i; i++) {
             System.out.println("Tu-Tu-Du-Du");
         }
 
     }
+    public void carGetRemainingRange () {
+        int remainingRange = carMaxFuelStorage - carFuelConsumption;
+        System.out.println(remainingRange * 100 +" km kann man noch fahren");
+    }
+
 }
--- src/at/kogler/oOProgramming/exercise01/MainFirstObject.java
+++ src/at/kogler/oOProgramming/exercise01/MainFirstObject.java
@@ -1,7 +1,9 @@
 package at.kogler.oOProgramming.exercise01;
+
 import at.kogler.oOProgramming.exercise01.CarBrummBrumm;
 
 public class MainFirstObject {
+
     public static void main(String[] args) {
         CarBrummBrumm car1 = new CarBrummBrumm();
         CarBrummBrumm car2 = new CarBrummBrumm();
@@ -13,6 +15,13 @@ public class MainFirstObject {
         car1.carFuelConsumption = 1;
         car1.carFuelInStorage = 27;
         car1.carMaxFuelStorage = 100;
+        car1.carBeFastAsFuckBoyyyyAKADriving();
+        car1.carGoHonk();
+        car1.carGoSlowAKABreak();
+        car1.carGoSuperBoostModeZefix();
+        car1.carMaxFuelChecker();
+        car1.carGetRemainingRange();
+
 
         car2.carID = "PO12545";
         car2.carBrand = "Porsche";
@@ -20,5 +29,11 @@ public class MainFirstObject {
         car2.carFuelConsumption = 6;
         car2.carFuelInStorage = 41;
         car2.carMaxFuelStorage = 167;
+        car2.carBeFastAsFuckBoyyyyAKADriving();
+        car2.carGoHonk();
+        car2.carGoSlowAKABreak();
+        car2.carGoSuperBoostModeZefix();
+        car2.carMaxFuelChecker();
+        car2.carGetRemainingRange();
     }
 }
