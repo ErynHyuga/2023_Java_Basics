911c242051c6a9bff3002ee2fe2711991f82fafb
ErynHyuga <you@example.com>
2023-11-15 09:31:45 +0100

    Completed the First step in assignment 4

--- src/at/kogler/oOProgramming/exercise01/CarBrummBrumm.java
+++ src/at/kogler/oOProgramming/exercise01/CarBrummBrumm.java
@@ -9,7 +9,17 @@ public class CarBrummBrumm {
     public int carFuelConsumption;
     public int carFuelInStorage;
     public int carMaxFuelStorage;
-    int speed;
+    public int speed;
+
+    public CarBrummBrumm(int carFuelConsumption, int carFuelInStorage, int carMaxFuelStorage, String carID, String carBrand, String carColour){
+        this.carColour = carColour;
+        this.carBrand = carBrand;
+        this.carID = carID;
+        this.carMaxFuelStorage = carMaxFuelStorage;
+        this.carFuelInStorage = carFuelInStorage;
+        this.carFuelConsumption = carFuelConsumption;
+    }
+
 
     public void carMaxFuelChecker() {
         if (carFuelInStorage > carMaxFuelStorage) {
@@ -19,10 +29,7 @@ public class CarBrummBrumm {
 
     public void carBeFastAsFuckBoyyyyAKADriving() {
         if (carFuelInStorage > 0) {
-            System.out.println("'Cause I'm crazy, hot and ready, but you like it\n" +
-                    "I wanna race for you (Shall I go now?)\n" +
-                    "Gas, gas, gas\n" +
-                    "I'm gonna step on the gas");
+            System.out.println("I'm gonna step on the gas" + "Gas, gas, gas");
             this.carFuelInStorage = this.carFuelInStorage - carFuelConsumption;
         } else {
             System.out.println("Car Cannot Move. Not Enough Fuel");
@@ -43,6 +50,7 @@ public class CarBrummBrumm {
     }
 
     public void carGoHonk() {
+        System.out.println("An 'Angerenter Hund' is driving in front of you, and is slow as fuck. How many times do you want to honk at this 'Biara'");
         var scanInput = new Scanner(System.in);
         int inputHonkRepitition = Integer.parseInt(scanInput.next());
         for (int i = 0; inputHonkRepitition >= i; i++) {
@@ -52,7 +60,12 @@ public class CarBrummBrumm {
     }
     public void carGetRemainingRange () {
         int remainingRange = carMaxFuelStorage - carFuelConsumption;
+        if(remainingRange < 0){
+            carAlarm();
+        }
         System.out.println(remainingRange * 100 +" km kann man noch fahren");
     }
-
+    public void carAlarm (){
+        System.out.println("ALAAAAAAAAAAAAAAAAAAAAAAAAAAARM");
+    }
 }
--- src/at/kogler/oOProgramming/exercise01/MainFirstObject.java
+++ src/at/kogler/oOProgramming/exercise01/MainFirstObject.java
@@ -5,16 +5,10 @@ import at.kogler.oOProgramming.exercise01.CarBrummBrumm;
 public class MainFirstObject {
 
     public static void main(String[] args) {
-        CarBrummBrumm car1 = new CarBrummBrumm();
-        CarBrummBrumm car2 = new CarBrummBrumm();
+        CarBrummBrumm car1 = new CarBrummBrumm(1,27,100,"PE125874", "Peugeot", "Munkili Bru");
+        CarBrummBrumm car2 = new CarBrummBrumm(12   ,41,167,"PO12545", "Porsche", "Fiery Rot");
 
 
-        car1.carID = "PE125874";
-        car1.carBrand = "Peugiot";
-        car1.carColour = "Munkili Bru";
-        car1.carFuelConsumption = 1;
-        car1.carFuelInStorage = 27;
-        car1.carMaxFuelStorage = 100;
         car1.carBeFastAsFuckBoyyyyAKADriving();
         car1.carGoHonk();
         car1.carGoSlowAKABreak();
@@ -23,12 +17,6 @@ public class MainFirstObject {
         car1.carGetRemainingRange();
 
 
-        car2.carID = "PO12545";
-        car2.carBrand = "Porsche";
-        car2.carColour = "Fiery Red";
-        car2.carFuelConsumption = 6;
-        car2.carFuelInStorage = 41;
-        car2.carMaxFuelStorage = 167;
         car2.carBeFastAsFuckBoyyyyAKADriving();
         car2.carGoHonk();
         car2.carGoSlowAKABreak();
