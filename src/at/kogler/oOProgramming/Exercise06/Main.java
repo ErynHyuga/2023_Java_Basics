7a4dc74d6ed39cdd4ceaaef8e28cb0b673f166d6
ErynHyuga <you@example.com>
2024-03-11 13:47:02 +0100

    started on handy

---
+++ src/at/kogler/oOProgramming/Exercise06/Main.java
@@ -0,0 +1,7 @@
+package at.kogler.oOProgramming.Exercise06;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/at/kogler/oOProgramming/Exercise06/SIM.java
@@ -0,0 +1,4 @@
+package at.kogler.oOProgramming.Exercise06;
+
+public class SIM {
+}
